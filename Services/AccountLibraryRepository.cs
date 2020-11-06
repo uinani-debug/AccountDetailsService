8d8dbc4db23ac9f4f28a53d0e784da90d982caea
maepocuser2 <maepocuser2@EC2AMAZ-UL0NSS6>
2020-11-06 10:18:07 +0000

    SQL to Oracle Migration changes

--- Services/AccountLibraryRepository.cs
+++ Services/AccountLibraryRepository.cs
@@ -8,6 +8,7 @@ using System.Data;
 
 using AccountLibrary.API.Models;
 using Microsoft.Data.SqlClient;
+using Oracle.ManagedDataAccess.Client;
 
 namespace AccountLibrary.API.Services
 {
@@ -15,12 +16,15 @@ namespace AccountLibrary.API.Services
     {
 
         private readonly string connstring;
-        private IDbConnection Connection => new SqlConnection(connstring);
+        private IDbConnection Connection => new OracleConnection(connstring);
         public AccountLibraryRepository()
         {
 
             //  connstring = "Server=192.168.0.164;Database=maecbsdb;user=SA;Password=TCSuser1123;Trusted_Connection=True;";
-            connstring = "Server=192.168.0.164;Database=maecbsdb;user=SA;Password=TCSuser1123;";
+            // connstring = "Server=192.168.0.164;Database=maecbsdb;user=SA;Password=TCSuser1123;";
+
+            connstring = "User Id=maeadmin;Password =Pa55w0rd;" +
+                "Data Source= 192.168.0.172:1521/orclpdb1";
         }
 
 
@@ -30,9 +34,9 @@ namespace AccountLibrary.API.Services
             {
                 c.Open();
                 var p = new DynamicParameters();
-                p.Add("accountNumber", accountNumber, DbType.String, ParameterDirection.Input);
+                p.Add(":accountNumber", accountNumber, DbType.String, ParameterDirection.Input);
 
-                string query = "select * from vw_customer where account_identifier= @accountNumber";
+                string query = "select * from vw_customer where account_identifier= :accountNumber";
                 var x = c.Query<Account>(query,p);
                 
                 //var test = c.Execute("sp_mae_get_customer_details", p, commandType: CommandType.StoredProcedure);
